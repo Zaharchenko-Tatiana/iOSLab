be4cd352c9e9298c1a2d6117960f67dbde9133a5
Zaharchenko-Tatiana <zaharchenkote@gmail.com>
2023-11-22 19:33:30 +0600

    Initial Commit

---
+++ Lab/LabApp.swift
@@ -0,0 +1,17 @@
+//
+//  LabApp.swift
+//  Lab
+//
+//  Created by Tatiana on 22.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct LabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
